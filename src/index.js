1cfd6f67964f7629a5b9bb0100a0a82938ab48d8
Sergey Shershnev <shv.sergey70@gmail.com>
2020-08-15 18:59:20 +0700

    added movie component & added prop validation

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
-ReactDOM.render(<App/>, document.querySelector(`#root`));
+const films = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `Revenant`];
+
+ReactDOM.render(<App films={films}/>, document.querySelector(`#root`));
